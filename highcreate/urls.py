5167d4ba4c975d826caa43de86d2f3273ed69f8f
Highcreate <junhui@highcreate.biz>
2019-05-04 00:20:20 +0900

    Update urls.py

--- highcreate/urls.py
+++ highcreate/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from homepage import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/',views.index),
+    path('add/', views.add),
 ]
