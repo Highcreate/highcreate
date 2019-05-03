bbc663285849d31f4c2b1f0200b567ccf5e4540a
Highcreate <junhui@highcreate.biz>
2019-05-04 00:20:40 +0900

    Update models.py

--- homepage/models.py
+++ homepage/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class userInfo(models.Model):
+     userName = models.CharField(primary_key=True, max_length=20, verbose_name='userName')
+     passWord = models.CharField(blank=True, max_length=20, verbose_name='passWord')
+
+     class Meta:
+         managed = False
+         db_table = 'userinfo'
