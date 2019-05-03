3a357159c1066a50d908ece89a67efec74b196c8
Highcreate <junhui@highcreate.biz>
2019-05-04 00:20:50 +0900

    Update views.py

--- homepage/views.py
+++ homepage/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from . import models
+from django.http import HttpResponse,HttpResponseRedirect
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+from django.template.response import TemplateResponse
+
+def index(request):
+    userInfos = models.userInfo.objects.all()
+    return render(request, 'index.html', {'userinfos':userInfos})
+
+
+# 保存数据
+@csrf_exempt
+def add(request):
+    username = request.POST['userName']
+    password = request.POST['passWord']
+    #models.userInfo.objects.create(username=username, password=password)
+    #return render(request, 'index.html')
+    userInfo = models.userInfo()
+    if len(username) > 0:
+        print("id不是null")
+        userInfo.userName = username;
+
+    userInfo.passWord = password
+    userInfo.save()
+    userInfos = models.userInfo.objects.all()
+    return HttpResponseRedirect("/index")
