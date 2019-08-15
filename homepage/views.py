5839b66f098b056d356142dc8475ab0dcd4ab5d0
HomePage <50070265+Highcreate@users.noreply.github.com>
2019-08-15 14:21:55 +0900

    Update views.py

--- homepage/views.py
+++ homepage/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
-from . import models
+from django.shortcuts import render,redirect
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponse,HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
-
 from django.template.response import TemplateResponse
+
+from homepage import models,forms
 
 def index(request):
     userInfos = models.userInfo.objects.all()
@@ -26,3 +28,27 @@ def add(request):
     userInfo.save()
     userInfos = models.userInfo.objects.all()
     return HttpResponseRedirect("/index")
+
+
+def simple_upload(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        mylife = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(mylife.name, mylife)
+        upload_file_url =fs.url(filename)
+        return render(request,'homepage/simple_upload.html', {
+            'uploaded_file_url':upload_file_url
+        })
+    return render(request,'homepage/simple_upload.html')
+
+def model_form_upload(request):
+    if request.method == 'POST':
+        form = forms.DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return  redirect('')
+    else:
+        form = forms.DocumentForm()
+    return render(request,'homepage/model_form_upload.html',{
+        'form':form
+    })
